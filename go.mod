aa2e3050730444058937ef27f666e6f11b9b6a88
Dennis Fanshaw <dennis.fanshaw@hyposcaler.io>
2023-03-10 22:12:40 +0000

    first commit

---
+++ go.mod
@@ -0,0 +1,21 @@
+module github.com/hyposcaler/ambientprobe
+
+go 1.20
+
+require (
+	github.com/quhar/bme280 v0.1.0
+	golang.org/x/exp v0.0.0-20230310171629-522b1b587ee0
+)
+
+require (
+	github.com/beorn7/perks v1.0.1 // indirect
+	github.com/cespare/xxhash/v2 v2.1.2 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/matttproud/golang_protobuf_extensions v1.0.1 // indirect
+	github.com/prometheus/client_golang v1.14.0 // indirect
+	github.com/prometheus/client_model v0.3.0 // indirect
+	github.com/prometheus/common v0.37.0 // indirect
+	github.com/prometheus/procfs v0.8.0 // indirect
+	golang.org/x/sys v0.1.0 // indirect
+	google.golang.org/protobuf v1.28.1 // indirect
+)
---
+++ main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"net/http"
+	"time"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"github.com/quhar/bme280"
+	"golang.org/x/exp/io/i2c"
+)
+
+var (
+	ambientTemperature = promauto.NewGauge(prometheus.GaugeOpts{
+		Name: "ambient_temperature",
+		Help: "The current Ambient Temperature",
+	})
+	ambientPressure = promauto.NewGauge(prometheus.GaugeOpts{
+		Name: "ambient_pressure",
+		Help: "The current Ambient Pressure",
+	})
+	ambientHumidity = promauto.NewGauge(prometheus.GaugeOpts{
+		Name: "ambient_humidity",
+		Help: "The current Ambient Humidity",
+	})
+)
+
+func main() {
+
+	d, err := i2c.Open(&i2c.Devfs{Dev: "/dev/i2c-1"}, bme280.I2CAddr)
+	if err != nil {
+		panic(err)
+	}
+
+	b := bme280.New(d)
+	err = b.Init()
+
+	if err != nil {
+		panic(err)
+	}
+
+	go func() {
+		for {
+			t, p, h, err := b.EnvData()
+			if err != nil {
+				panic(err)
+			}
+			ambientTemperature.Set(t)
+			ambientPressure.Set(p)
+			ambientHumidity.Set(h)
+			// fmt.Printf("Temp: %fC, Press: %fhPa, Hum: %f%%\n", t, p, h)
+			time.Sleep(5 * time.Second)
+		}
+	}()
+
+	http.Handle("/metrics", promhttp.Handler())
+	http.ListenAndServe(":2323", nil)
+
+}
